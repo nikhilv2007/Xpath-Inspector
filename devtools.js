14ec11e173d49977983316512f549b2b933e5356
nikhilv2007 <nikhilv_2007@yahoo.co.in>
2015-02-27 17:39:33 +0530

    1
    initial commit 1

---
+++ devtools.js
@@ -0,0 +1,19 @@
+
+chrome.devtools.panels.create("Webdriver",
+    null,
+    "panel/inspectorPanel.html",
+    function(panel) {
+      // code invoked on panel creation
+      
+    }
+);
+
+chrome.devtools.inspectedWindow.eval(
+          "jQuery.fn.jquery",
+           function(result, isException) {
+             if (isException)
+               console.log("the page is not using jQuery");
+             else
+               console.log("The page is using jQuery v" + result);
+           }
+      );
